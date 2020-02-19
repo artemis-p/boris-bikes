305a74a5ab88eaed7fb2b9f7d125594c8b1c13e8
Student <student@Makerss-MacBook-Air-2.local>
2020-02-19 17:15:43 +0000

    added and passed test to release and check if bike is working

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,4 +5,7 @@ describe "DockingStation" do
     expect(DockingStation.new).to respond_to(:release_bike)
     # expect{DockingStation.new.release_bike}.not_to raise_error
   end
+  it "releases working bike" do
+    expect(DockingStation.new.release_bike).to be_working
+  end
 end
